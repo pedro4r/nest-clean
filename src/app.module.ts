f370d35f9383d8f40bf2068ee5388830870c5bbb
Pedro Requiao <alonsofts@gmail.com>
2024-01-15 18:01:19 -0500

    add: protect routes with guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,25 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
-import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from 'src/env'
 import { AuthModule } from './auth/auth.module'
+import { CreateAccountController } from './controllers/create-account.controller'
 import { AuthenticateController } from './controllers/authenticate.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            validate: (env) => envSchema.parse(env),
-            isGlobal: true,
-        }),
-        AuthModule,
-    ],
-    controllers: [CreateAccountController, AuthenticateController],
-    providers: [PrismaService],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { JwtModule } from '@nestjs/jwt'
-import { PassportModule } from '@nestjs/passport'
 import { Env } from 'src/env'
+import { JwtStrategy } from './jwt.strategy'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
     imports: [
@@ -24,5 +25,6 @@ import { Env } from 'src/env'
             },
         }),
     ],
+    providers: [JwtStrategy],
 })
 export class AuthModule {}
