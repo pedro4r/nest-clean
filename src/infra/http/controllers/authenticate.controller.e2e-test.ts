e377388ff5297abc4f25ee8fdd561c783d81f7ba
Pedro Requiao <alonsofts@gmail.com>
2024-01-20 01:13:37 -0500

    feat: refactoring tests

--- src/infra/http/controllers/authenticate.controller.e2e-test.ts
+++ src/infra/http/controllers/authenticate.controller.e2e-test.ts
@@ -1,46 +1,42 @@
-
 import { AppModule } from '@/infra/app.module'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { DatabaseModule } from '@/infra/database/datbase.module'
 import { INestApplication } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
 import { hash } from 'bcryptjs'
 import request from 'supertest'
+import { StudentFactory } from 'test/factories/make-student'
 
 describe('Authenticate (E2E)', () => {
-    let app: INestApplication
-    let prisma: PrismaService
+  let app: INestApplication
+  let studentFactory: StudentFactory
 
-    beforeAll(async () => {
-        const moduleRef = await Test.createTestingModule({
-            imports: [AppModule],
-        }).compile()
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory],
+    }).compile()
 
-        app = moduleRef.createNestApplication()
+    app = moduleRef.createNestApplication()
 
-        prisma = moduleRef.get(PrismaService)
+    studentFactory = moduleRef.get(StudentFactory)
 
-        await app.init()
+    await app.init()
+  })
+
+  test('[POST] /sessions', async () => {
+    await studentFactory.makePrismaStudent({
+      email: 'johndoe@example.com',
+      password: await hash('123456', 8),
     })
 
-    test('[POST] /sessions', async () => {
-        await prisma.user.create({
-            data: {
-                name: 'John Doe',
-                email: 'johndoe@example.com',
-                password: await hash('123456', 8),
-            },
-        })
-
-        const response = await request(app.getHttpServer())
-            .post('/sessions')
-            .send({
-                email: 'johndoe@example.com',
-                password: '123456',
-            })
-
-        expect(response.statusCode).toBe(201)
-        expect(response.body).toEqual({
-            access_token: expect.any(String),
-        })
+    const response = await request(app.getHttpServer()).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
     })
+
+    expect(response.statusCode).toBe(201)
+    expect(response.body).toEqual({
+      access_token: expect.any(String),
+    })
+  })
 })
