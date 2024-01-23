22ba2bc904f57d99782ba552264b9158c2639620
Pedro Requiao <alonsofts@gmail.com>
2024-01-22 21:53:36 -0500

    add: questions with attachments

--- test/repositories/in-memory-question-attachments-repository.ts
+++ test/repositories/in-memory-question-attachments-repository.ts
@@ -2,21 +2,35 @@ import { QuestionAttachmentsRepository } from '@/domain/forum/application/reposi
 import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
 
 export class InMemoryQuestionAttachmentsRepository
-    implements QuestionAttachmentsRepository
+  implements QuestionAttachmentsRepository
 {
-    public items: QuestionAttachment[] = []
+  public items: QuestionAttachment[] = []
 
-    async findManyByQuestionId(questionId: string) {
-        const questionAttachments = this.items.filter(
-            (item) => item.questionId.toString() === questionId
-        )
-        return questionAttachments
-    }
+  async findManyByQuestionId(questionId: string) {
+    const questionAttachments = this.items.filter(
+      (item) => item.questionId.toString() === questionId,
+    )
 
-    async deleteManyByQuestionId(questionId: string) {
-        const questionAttachments = this.items.filter(
-            (item) => item.questionId.toString() !== questionId
-        )
-        this.items = questionAttachments
-    }
+    return questionAttachments
+  }
+
+  async createMany(attachments: QuestionAttachment[]): Promise<void> {
+    this.items.push(...attachments)
+  }
+
+  async deleteMany(attachments: QuestionAttachment[]): Promise<void> {
+    const questionAttachments = this.items.filter((item) => {
+      return !attachments.some((attachment) => attachment.equals(item))
+    })
+
+    this.items = questionAttachments
+  }
+
+  async deleteManyByQuestionId(questionId: string) {
+    const questionAttachments = this.items.filter(
+      (item) => item.questionId.toString() !== questionId,
+    )
+
+    this.items = questionAttachments
+  }
 }
